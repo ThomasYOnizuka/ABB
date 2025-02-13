f248e3b0fc796a124338244ceb4da852908146e3
Thomas O. <thomas.onizuka@unifesp.br>
2025-02-13 13:20:23 -0300

    Update arvore ABB.c

--- arvore ABB.c
+++ arvore ABB.c
@@ -1,82 +1,280 @@
+// arvore.c - implementação de árvore binária
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 
-// Estrutura para armazenar os dados de cada registro
-typedef struct {
-    char nome[100];
-    char cargo[100];
-    char uorg_lotacao[100];
-} Registro;
+#include "arvore.h"
 
-// Estrutura de um nó da árvore binária de busca
-typedef struct Node {
-    Registro registro;
-    struct Node *esq, *dir;
-} Node;
+NO *pai(NO *raiz, NO *noI) {
 
-// Função para criar um novo nó
-Node* novoNo(Registro registro) {
-    Node* temp = (Node*)malloc(sizeof(Node));
-    temp->registro = registro;
-    temp->esq = temp->dir = NULL;
-    return temp;
+    if( raiz == noI ) return NULL;
+
+    if( raiz->valor > noI->valor ) {    // está para o lado esquerdo
+        if( noI == raiz->esq )          // raiz é pai do noI
+            return raiz;
+        return pai( raiz->esq, noI );
+    }
+    else {                             // está para o lado  direito
+        if( noI == raiz->dir )
+            return raiz;
+        return pai( raiz->dir, noI);
+    }
 }
 
-// Função para inserir um novo registro na ABB
-Node* inserir(Node* node, Registro registro) {
-    if (node == NULL) return novoNo(registro);
+NO *criaNo(int val) {
+    NO *novoNo;
 
-    if (strcmp(registro.nome, node->registro.nome) < 0)
-        node->esq = inserir(node->esq, registro);
-    else if (strcmp(registro.nome, node->registro.nome) > 0)
-        node->dir = inserir(node->dir, registro);
-
-    return node;
+    novoNo = malloc( sizeof(NO));
+    if(!novoNo) {
+        printf("Erro de alocação de memória.\n");
+        exit(-1);
+    }
+    novoNo->valor = val;
+    novoNo->esq = NULL;
+    novoNo->dir = NULL;
+    return novoNo;
 }
 
-// Função para buscar um registro pelo nome
-Node* buscar(Node* root, char nome[]) {
-    if (root == NULL || strcmp(root->registro.nome, nome) == 0)
-        return root;
 
-    if (strcmp(nome, root->registro.nome) < 0)
-        return buscar(root->esq, nome);
 
-    return buscar(root->dir, nome);
+NO *insereNo(NO *raiz, int val ) {
+
+    int fb;
+
+    if( raiz==NULL) {
+       return criaNo(val);
+    }
+    if( val > raiz->valor )    // maior
+        raiz->dir = insereNo( raiz->dir, val);
+    else if (val < raiz->valor) {
+        raiz->esq = insereNo( raiz->esq, val);
+    }
+    else return raiz;      // Chave duplicada não permitida
+
+    fb = FB(raiz);
+
+    if( fb>1 ) {
+        if ( FB(raiz->dir) >= 0)
+            raiz = rotacaoEsquerda(raiz);
+        else
+            raiz = rotacaoDuplaEsquerda(raiz);
+    }
+    if( fb<-1 ){
+        if ( FB(raiz->esq) >= 0)
+            raiz = rotacaoDireita(raiz);
+        else
+            raiz = rotacaoDuplaDireita(raiz);
+
+    }
+
+    return raiz;
 }
+
+NO *ehAVL( NO *raiz ) {
+    int fb;
+    NO *p;
+    if(raiz==NULL) return NULL;
+    fb = FB(raiz);
+    if(fb>1 || fb<-1)
+        return raiz;
+    p = ehAVL( raiz->esq);
+    if(p) return p;
+    p = ehAVL(raiz->dir);
+    if(p) return p;
+    return NULL;
+}   
+
+NO *proximo(NO *raiz, NO *no) {
+    NO *p;
+
+    if(no->dir != NULL) {
+        p = no->dir;
+        while( p->esq != NULL )
+            p = p->esq;
+        return p;
+    }
+
+    p = pai(raiz, no);
+    while(p != NULL && p->dir == no )
+        no = p;
+        p = (raiz, p );
+    return p;
+}
+
+void imprimeERD(NO *raiz) {
+    if(raiz!=NULL) {
+        imprimeERD( raiz->esq );
+        printf("%d\n",raiz->valor);
+        imprimeERD( raiz->dir );
+    }
+}
+
+void imprimeDRE(NO *raiz) {
+    if(raiz!=NULL) {  
+        imprimeDRE( raiz->dir );
+        printf("%d\n",raiz->valor);
+        imprimeDRE( raiz->esq );
+    }
+}
+
+int altura(NO *no) {
+    int aEsq, aDir;
+
+    if( no==NULL )
+        return -1;    
+    aEsq = altura(no->esq);
+    aDir = altura(no->dir);
+
+    if(aEsq>aDir) 
+        return aEsq+1;
+    else
+        return aDir+1;
+}
+
+int FB(NO *no) {  // Fator de Balanceamento
+    if(!no) return 0;
+    return altura( no->dir ) - altura( no->esq );
+}
+
+NO *rotacaoDireita( NO *raiz ){
+    NO *novaRaiz;
+    novaRaiz = raiz->esq;
+    raiz->esq = novaRaiz->dir;
+    novaRaiz->dir = raiz;
+    return novaRaiz;
+}
+NO *rotacaoEsquerda( NO *raiz ){
+    NO *novaRaiz;
+    novaRaiz = raiz->dir;
+    raiz->dir = novaRaiz->esq;
+    novaRaiz->esq = raiz;
+    return novaRaiz;
+}
+NO *rotacaoDuplaDireita( NO *raiz ) {
+    raiz->esq = rotacaoEsquerda( raiz->esq );
+    return rotacaoDireita( raiz );
+}
+NO *rotacaoDuplaEsquerda( NO *raiz ) {
+    raiz->dir = rotacaoDireita( raiz->dir );
+    return rotacaoEsquerda( raiz );
+}
+int profundidade(NO *raiz, NO *noI) {
+    if(raiz==NULL) return -1;
+    if( raiz->valor == noI->valor )
+        return 0;
+    if( raiz->valor > noI->valor )
+        return profundidade( raiz->esq, noI) + 1;
+    else
+        return profundidade( raiz->dir, noI) + 1;
+}
+
+NO *removeNo( NO *raiz ) {
+    NO *p, *q;
+
+    if( raiz->esq==NULL) q=raiz->dir;
+    else {
+        p = raiz;
+        q = raiz->esq;
+        while( q->dir!=NULL ){
+            p = q;
+            q = q->dir;
+        }
+        if( p!= raiz) {
+            p->dir = q->esq;
+            q->esq = raiz->esq;
+        }
+        q->dir = raiz->dir;
+    }
+    free(raiz);
+    return q;
+}
+
+void removeNoVal(NO **raiz, int valor) {
+    NO *s, *p;
+    
+    s = busca(*raiz, valor);
+    
+    if( s==*raiz ){
+        *raiz = removeNo(s);
+    }
+    else {
+        p = pai(*raiz, s);
+        if( s ==  p->esq){
+            p->esq = removeNo(p->esq);
+        }
+        else   {
+            p->dir  = removeNo(p->dir);
+        }
+    }
+}
+
+NO *busca( NO *no, int val ){
+    if( no==NULL || no->valor == val ) 
+        return no;
+    if( no->valor > val )
+        return busca( no->esq, val );
+    else   
+        return busca( no->dir, val );
+}
+
+void imprimeNivel(NO *raiz, int alturaDesejada, int alturaCorrente){
+    if(raiz==NULL) return;
+
+    if( alturaDesejada == alturaCorrente) {
+        printf("%d ", raiz->valor);
+        return;
+    }   
+    imprimeNivel(raiz->esq, alturaDesejada, alturaCorrente+1 );
+    imprimeNivel(raiz->dir, alturaDesejada, alturaCorrente+1 );
+}
+
+void imprimeIrmaos(NO *raiz, NO *noI){
+    int p;
+    p = profundidade(raiz,noI);
+    imprimeNivel(raiz, p, 0);
+}
+
 
 int main() {
-    Node* root = NULL;
-    char nomeBusca[100];
+    NO *raiz = NULL;  // árvore vazia
+    NO *s,*t;
 
-    // Simulação de leitura do CSV e inserção na ABB
-    FILE *file = fopen("https://www.bioinfo.unifesp.br/datasets/ed2024/Unifesp-24r.csv", "r");
-    if (file == NULL) {
-         perror("Erro ao abrir o arquivo");
-         return EXIT_FAILURE;
-     }
-    // Código para ler o arquivo e inserir registros na ABB
-     fclose(file);
+    // raiz = insereNo(raiz, 6);
+    // raiz = insereNo(raiz, 7);
+    // raiz = insereNo(raiz, 9);
+    // raiz = insereNo(raiz, 12);
+    // raiz = insereNo(raiz, 2);
+    // raiz = insereNo(raiz, 15);
+    // raiz = insereNo(raiz, 1); 
+    // raiz = insereNo(raiz, 14);
+    // raiz = insereNo(raiz, 8);
+    // raiz = insereNo(raiz, 3);
+    // raiz = insereNo(raiz, 4);
+    // raiz = insereNo(raiz, 11);
+    // raiz = insereNo(raiz, 10);
+    // raiz = insereNo(raiz, 13);
+    // raiz = insereNo(raiz, 5);
 
-    // Simulando a inserção de registros
-    Registro r1 = {"Alice Silva", "Engenheira", "Departamento A"};
-    Registro r2 = {"Carlos Souza", "Analista", "Departamento B"};
-    root = inserir(root, r1);
-    root = inserir(root, r2);
+    raiz = insereNo(raiz, 1);
+    raiz = insereNo(raiz, 2);
+    raiz = insereNo(raiz, 3);
+    raiz = insereNo(raiz, 4);
+    raiz = insereNo(raiz, 5);
+    raiz = insereNo(raiz, 6);
+    raiz = insereNo(raiz, 7);
+    raiz = insereNo(raiz, 8);
+    raiz = insereNo(raiz, 9);
+    raiz = insereNo(raiz, 10);
+    raiz = insereNo(raiz, 11);
 
-    // Solicitar nome para busca
-    printf("Digite o nome completo para busca: ");
-    fgets(nomeBusca, sizeof(nomeBusca), stdin);
-    nomeBusca[strcspn(nomeBusca, "\n")] = '\0';  // Remover newline
 
-    Node* resultado = buscar(root, nomeBusca);
-    if (resultado != NULL) {
-        printf("Cargo: %s\n", resultado->registro.cargo);
-        printf("UORG_LOTACAO: %s\n", resultado->registro.uorg_lotacao);
-    } else {
-        printf("Registro não encontrado.\n");
+    s = ehAVL(raiz);
+    if(s) {
+        printf("Nó %d  FB: %d", s->valor, FB(s));
     }
+    printf("\nAltura da árvore: %d\n\n", altura(raiz));
+}
 
     return 0;
 }
